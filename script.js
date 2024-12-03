4a522a7334b8dad904fea3783640448fdd7a9525
Vishal Chaudhari <122671523+vishal0714@users.noreply.github.com>
2024-12-03 13:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// Add the getKeys method to Object.prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Create the student object with a name property
+const student = {
+  name: "John Doe",
+};
